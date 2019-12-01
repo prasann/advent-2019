e195ec6524830f8c7a1bcee1b42043d8a0d644e8
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2019-12-01 23:54:38 +0530

    Second start

--- src/main/kotlin/advent/D01.kt
+++ src/main/kotlin/advent/D01.kt
@@ -4,15 +4,37 @@ import java.io.File
 import kotlin.math.floor
 
 class D01 {
-    private fun operation(num: Double): Double {
+    private fun op(num: Double): Double {
+        if (num <= 0) {
+            throw error("Negative")
+        }
         return floor((num / 3)) - 2
     }
 
-    fun main(): Double {
+    fun calculate(inputFilePath: String): Double {
         var sum = 0.0
-        File("src/main/resources/D01.txt").forEachLine {
-            sum += operation(it.toDouble())
+        File(inputFilePath).forEachLine {
+            sum += op(it.toDouble())
         }
         return sum
+    }
+
+    fun calculateExtension(inputFilePath: String): Double {
+        var sum = 0.0
+        File(inputFilePath).forEachLine {
+            sum += extendedOp(it.toDouble())
+        }
+        return sum
+    }
+
+    private fun extendedOp(initNum: Double, result: Double = 0.0): Double {
+        if (initNum <= 0.0) {
+            return result
+        }
+        val singleOpResult = op(initNum)
+        if (singleOpResult <= 0) {
+            return result
+        }
+        return extendedOp(singleOpResult, result + singleOpResult)
     }
 }
--- src/test/kotlin/advent/D01Test.kt
+++ src/test/kotlin/advent/D01Test.kt
@@ -2,9 +2,14 @@ package advent
 
 import org.junit.jupiter.api.Test
 
-public class D01Test {
+class D01Test {
     @Test
-    fun testingMain() {
-        println(D01().main())
+    fun part1() {
+        println(D01().calculate("src/main/resources/D01P01.txt"))
+    }
+
+    @Test
+    fun part2() {
+        println(D01().calculateExtension("src/main/resources/D01P01.txt"))
     }
 }
