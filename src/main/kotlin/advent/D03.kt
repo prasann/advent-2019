5aeeaa8c8a643655f80e541df576431056d2b0e1
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2019-12-04 21:29:22 +0530

    sixth star

--- src/main/kotlin/advent/D03.kt
+++ src/main/kotlin/advent/D03.kt
@@ -4,35 +4,35 @@ import kotlin.math.abs
 
 class D03 {
     fun generateSeq(x1: Int, y1: Int, x2: Int, y2: Int): List<String> {
-
         return if (x1 != x2) {
-            if (x1 < x2) (x1..x2).map { "$it,$y1" } else (x1 downTo x2).map { "$it,$y1" }
+            if (x1 < x2) (x1 + 1..x2).map { "$it,$y1" } else (x1 - 1 downTo x2).map { "$it,$y1" }
         } else {
-            if (y1 < y2) (y1..y2).map { "$x1,$it" } else (y1 downTo y2).map { "$x1,$it" }
+            if (y1 < y2) (y1 + 1..y2).map { "$x1,$it" } else (y1 - 1 downTo y2).map { "$x1,$it" }
         }
     }
 
     fun parse(wirePath: String): List<String> {
-        val result = mutableListOf<String>()
+        val result = mutableListOf<String>("0,0")
         var (x, y) = arrayOf(0, 0)
         wirePath.split(",").map {
-            var count = it.removeRange(0..0)
+            val count = it.removeRange(0..0)
+            val stepCounter = Integer.parseInt(count)
             when (it[0]) {
                 'U' -> {
-                    result.addAll(generateSeq(x, y, x, y + Integer.parseInt(count)))
-                    y += Integer.parseInt(count)
+                    result.addAll(generateSeq(x, y, x, y + stepCounter))
+                    y += stepCounter
                 }
                 'R' -> {
-                    result.addAll(generateSeq(x, y, x + Integer.parseInt(count), y))
-                    x += Integer.parseInt(count)
+                    result.addAll(generateSeq(x, y, x + stepCounter, y))
+                    x += stepCounter
                 }
                 'D' -> {
-                    result.addAll(generateSeq(x, y, x, y - Integer.parseInt(count)))
-                    y -= Integer.parseInt(count)
+                    result.addAll(generateSeq(x, y, x, y - stepCounter))
+                    y -= stepCounter
                 }
                 'L' -> {
-                    result.addAll(generateSeq(x, y, x - Integer.parseInt(count), y))
-                    x -= Integer.parseInt(count)
+                    result.addAll(generateSeq(x, y, x - stepCounter, y))
+                    x -= stepCounter
                 }
                 else -> throw RuntimeException("Not allowed")
             }
@@ -41,22 +41,22 @@ class D03 {
     }
 
 
-    fun compute(wirePath1: String, wirePath2: String): Int {
-        println("Started !!")
+    fun compute(wirePath1: String, wirePath2: String): Map<String, Int> {
         val wire1Nodes = parse(wirePath1)
-        println("Done listing all nodes for path1")
         val wire2Nodes = parse(wirePath2)
-        println("Done listing all nodes for path2")
 
         val distance = mutableListOf<Int>()
-        wire1Nodes.forEach {
-            val indexOf = wire2Nodes.indexOf(it)
-            if (indexOf != -1 && indexOf != 0) {
-                val (x, y) = it.split(",").map { num -> Integer.parseInt(num) }
+        val steps = mutableListOf<Int>()
+        wire1Nodes.forEachIndexed { in1, item ->
+            val in2 = wire2Nodes.indexOf(item)
+            if (in2 != -1 && in2 != 0) {
+                steps.add(in1 + in2)
+                val (x, y) = item.split(",").map { num -> Integer.parseInt(num) }
                 distance.add(abs(x) + abs(y))
             }
         }
         distance.sort()
-        return distance[0]
+        steps.sort()
+        return mapOf("distance" to distance[0], "steps" to steps[0])
     }
 }
