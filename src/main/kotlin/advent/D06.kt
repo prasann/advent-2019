83810703650abe903cbd23ed826e7e50410b1334
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2019-12-08 23:00:39 +0530

    12th star

--- src/main/kotlin/advent/D06.kt
+++ src/main/kotlin/advent/D06.kt
@@ -1,13 +1,23 @@
 package advent
 
-class D06 {
-    private tailrec fun count(value: String, dataMap: Map<String, String>, orbits: Int = 1): Int {
-        if (value == "COM") return orbits
-        return count(dataMap.getValue(value), dataMap, orbits + 1)
+class D06(input: List<String>) {
+    private tailrec fun Map<String, String>.orbitChain(body: String, chain: List<String> = emptyList()): List<String> =
+        if (containsKey(body)) orbitChain(getValue(body), chain + body) else chain
+
+    private val orbitMap = input.map { it.split(')') }.map { it[1] to it[0] }.toMap()
+
+    fun solvePart1(): Int {
+        return orbitMap.keys.map { orbitMap.orbitChain(it).size }.sum()
     }
 
-    fun solvePart1(input: List<String>): Int {
-        val orbitMap = input.map { it.split(')') }.map { it[1] to it[0] }.toMap()
-        return orbitMap.map { count(it.value, orbitMap) }.sum()
+    fun solvePart2(): Int {
+        val youMap = orbitMap.orbitChain("YOU").reversed()
+        val santaMap = orbitMap.orbitChain("SAN").reversed()
+
+        val commonLength = youMap.indices.takeWhile {
+            youMap[it] == santaMap[it]
+        }.size
+
+        return youMap.size + santaMap.size - commonLength * 2 - 2
     }
 }
--- src/test/kotlin/advent/D06Test.kt
+++ src/test/kotlin/advent/D06Test.kt
@@ -8,6 +8,12 @@ class D06Test {
     @Test
     fun solve1() {
         val input = File("src/main/resources/D06P01.txt").readLines()
-        println(D06().solvePart1(input))
+        println(D06(input).solvePart1())
+    }
+
+    @Test
+    fun solve2() {
+        val input = File("src/main/resources/D06P02.txt").readLines()
+        println(D06(input).solvePart2())
     }
 }
